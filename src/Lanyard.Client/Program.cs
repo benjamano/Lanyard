d9835bc14c4b61d5087a60c10ef38fdfd9a53922
Ben Mercer <benmercer76@btinternet.com>
2026-06-17 21:43:58 +0100

    Fixed a bug

--- src/Lanyard.Client/Program.cs
+++ src/Lanyard.Client/Program.cs
@@ -113,59 +113,59 @@ async Task Main()
     await signalRClient.Connect(registrations);
     await laserGameStatePublisher.PublishAsync();
 
-    IPacketSniffer sniffer = provider.GetRequiredService<IPacketSniffer>();
+    // IPacketSniffer sniffer = provider.GetRequiredService<IPacketSniffer>();
 
-    await sniffer.StartSniffingAsync();
+    // await sniffer.StartSniffingAsync();
 
-    bool stop = false;
+    // bool stop = false;
 
-    while (stop == false)
-    {
-        string? message = Console.ReadLine();
+    // while (stop == false)
+    // {
+    //     string? message = Console.ReadLine();
 
-        if (string.IsNullOrWhiteSpace(message) == false)
-        {
-            if (int.TryParse(message, out int actionType))
-            {
-                switch (actionType)
-                {
-                    case 1:
-                        // SEND A TEST PACKET WITH 10 MINUTES REMAINING
+    //     if (string.IsNullOrWhiteSpace(message) == false)
+    //     {
+    //         if (int.TryParse(message, out int actionType))
+    //         {
+    //             switch (actionType)
+    //             {
+    //                 case 1:
+    //                     // SEND A TEST PACKET WITH 10 MINUTES REMAINING
 
-                        Random random = new();
+    //                     Random random = new();
 
-                        sniffer.HandlePacketAsync(["4", "@015", "0"]);
+    //                     sniffer.HandlePacketAsync(["4", "@015", "0"]);
 
-                        sniffer.HandlePacketAsync(["1", "0", "0", "600"]);
+    //                     sniffer.HandlePacketAsync(["1", "0", "0", "600"]);
 
-                        sniffer.HandlePacketAsync(["3", "1", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
+    //                     sniffer.HandlePacketAsync(["3", "1", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
 
-                        sniffer.HandlePacketAsync(["2", "0", random.Next(1, 101).ToString()]);
+    //                     sniffer.HandlePacketAsync(["2", "0", random.Next(1, 101).ToString()]);
 
-                        sniffer.HandlePacketAsync(["2", "2", random.Next(1, 101).ToString()]);
+    //                     sniffer.HandlePacketAsync(["2", "2", random.Next(1, 101).ToString()]);
 
-                        sniffer.HandlePacketAsync(["3", "3", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
+    //                     sniffer.HandlePacketAsync(["3", "3", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
 
-                        sniffer.HandlePacketAsync(["3", "7", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
+    //                     sniffer.HandlePacketAsync(["3", "7", "0", random.Next(1, 201).ToString(), "0", "0", "0", random.Next(1, 101).ToString()]);
 
-                        sniffer.HandlePacketAsync(["4", "@014", "0"]);
+    //                     sniffer.HandlePacketAsync(["4", "@014", "0"]);
 
-                        break;
-                    default:
-                        Console.WriteLine("Unknown action.");
-                        break;
-                }
-            }
-            else
-            {
-                stop = true;
-            }
-        }
-        else
-        {
-            stop = true;
-        }
-    }
+    //                     break;
+    //                 default:
+    //                     Console.WriteLine("Unknown action.");
+    //                     break;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             stop = true;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         stop = true;
+    //     }
+    // }
 }
 
 void ShowControls()
