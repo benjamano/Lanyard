9b70b75c278cce8c9c9ad3bf1d858ff86b782866
Ben Mercer <benmercer76@btinternet.com>
2026-08-23 00:11:38 +0100

    Removed Phone Number and DOB

--- src/Lanyard.Infrastructure/Models/ApplicationUserModels.cs
+++ src/Lanyard.Infrastructure/Models/ApplicationUserModels.cs
@@ -7,7 +7,6 @@ namespace Lanyard.Infrastructure.Models
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public DateTime? DateOfBirth { get; set; }
 
         public string? PreferredCulture { get; set; }
 
